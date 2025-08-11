c8d1d52e7e5974c29d51048f0511ce623e27bc93
一之濑亚子 <iznyaku@outlook.jp>
2025-08-11 16:13:29 +0800

    完成数据抓取部分

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import json
 
 import requests
 
-re = requests.get('http://127.0.0.1:5000/scut/jwnotice')
+re = requests.get('http://127.0.0.1:5000/scut/jwnotice?name=jw&pageSize=75')
 js = json.loads(re.text)
 print(js)
 
