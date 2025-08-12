b3b2c8a583e94f73118e8b35be708b6e1aa1063e
一之濑亚子 <iznyaku@outlook.jp>
2025-08-12 14:37:39 +0800

    加入最新更新时间的获取函数

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import json
 
 import requests
 
-re = requests.get('http://127.0.0.1:5000/scut/jwnotice?name=jw&pageSize=75')
+re = requests.get('http://127.0.0.1:5001/lastUpdated')
 js = json.loads(re.text)
 print(js)
 
